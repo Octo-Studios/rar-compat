cc705c922dc6fe0795c7a7a4322033cbd68a34b8
iuak2 <iuak228@gmail.com>
2025-02-17 23:06:19 +0300

    Updated logic working mimic.

--- src/main/java/it/hurts/octostudios/rarcompat/handlers/MimicHandler.java
+++ src/main/java/it/hurts/octostudios/rarcompat/handlers/MimicHandler.java
@@ -1,15 +1,19 @@
 package it.hurts.octostudios.rarcompat.handlers;
 
 import artifacts.entity.MimicEntity;
-import it.hurts.octostudios.rarcompat.config.MimicConfigData;
-import it.hurts.octostudios.rarcompat.init.ConfigRegistry;
+import it.hurts.octostudios.rarcompat.RARCompat;
+import it.hurts.sskirillss.relics.items.relics.base.IRelicItem;
+import net.minecraft.core.Holder;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.living.LivingDropsEvent;
+import net.neoforged.neoforge.event.server.ServerStartedEvent;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,8 +22,10 @@ import java.util.List;
 public class MimicHandler {
     public static final List<Item> ITEMS = new ArrayList<>();
 
-    public static void readFromConfig(MimicConfigData data) {
-        var entries = data.getItems();
+    @SubscribeEvent
+    public static void onStartedServer(ServerStartedEvent event) {
+        var entries = BuiltInRegistries.ITEM.getTag(TagKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(RARCompat.MODID, "mimic_drops")))
+                .stream().flatMap(holderSet -> holderSet.stream().map(Holder::value)).toList();
 
         MimicHandler.ITEMS.clear();
 
@@ -27,17 +33,13 @@ public class MimicHandler {
             return;
 
         for (var entry : entries) {
-            var id = ResourceLocation.parse(entry);
+            var id = ResourceLocation.parse(String.valueOf(entry));
 
-            if (!BuiltInRegistries.ITEM.containsKey(id))
+            if (!BuiltInRegistries.ITEM.containsKey(id) || !(BuiltInRegistries.ITEM.get(id) instanceof IRelicItem))
                 continue;
 
             MimicHandler.ITEMS.add(BuiltInRegistries.ITEM.get(id));
         }
-    }
-
-    public static void init() {
-        readFromConfig(ConfigRegistry.MIMIC_CONFIG);
     }
 
     @SubscribeEvent
