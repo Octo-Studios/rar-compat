30184fbb66d240d75508be21a615153caace8243
SyntaX <140329231+SyntaX0x@users.noreply.github.com>
2025-10-04 22:19:26 +0200

    Add temporary fix for #43
    All non-backported artifact items now work correctly

--- src/main/java/it/hurts/octostudios/rarcompat/mixin/ModGameRulesDoubleMixin.java
+++ src/main/java/it/hurts/octostudios/rarcompat/mixin/ModGameRulesDoubleMixin.java
@@ -10,6 +10,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class ModGameRulesDoubleMixin {
     @Inject(method = "get*", at = @At("HEAD"), cancellable = true, remap = false)
     private void disableAllDoubleGameRules(CallbackInfoReturnable<Double> cir) {
+        ModGameRules.DoubleValue instance = (ModGameRules.DoubleValue) (Object) this;
+
+        if (instance.integerValue().equals(ModGameRules.CHORUS_TOTEM_TELEPORTATION_CHANCE.integerValue()) ||
+                instance.integerValue().equals(ModGameRules.CLOUD_IN_A_BOTTLE_SPRINT_JUMP_VERTICAL_VELOCITY.integerValue()) ||
+                instance.integerValue().equals(ModGameRules.CLOUD_IN_A_BOTTLE_SPRINT_JUMP_HORIZONTAL_VELOCITY.integerValue())) {
+            return;
+        }
+
         cir.setReturnValue(0.0);
     }
 }
--- src/main/java/it/hurts/octostudios/rarcompat/mixin/ModGameRulesIntegerMixin.java
+++ src/main/java/it/hurts/octostudios/rarcompat/mixin/ModGameRulesIntegerMixin.java
@@ -10,6 +10,26 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 class ModGameRulesIntegerMixin {
     @Inject(method = "get*", at = @At("HEAD"), cancellable = true, remap = false)
     private void disableAllIntegerGameRules(CallbackInfoReturnable<Integer> cir) {
+        ModGameRules.IntegerValue instance = (ModGameRules.IntegerValue) (Object) this;
+
+        if (instance.equals(ModGameRules.ETERNAL_STEAK_COOLDOWN) ||
+                instance.equals(ModGameRules.EVERLASTING_BEEF_COOLDOWN) ||
+                instance.equals(ModGameRules.CRYSTAL_HEART_HEALTH_BONUS) ||
+                instance.equals(ModGameRules.CHORUS_TOTEM_HEALTH_RESTORED) ||
+                instance.equals(ModGameRules.CHORUS_TOTEM_COOLDOWN) ||
+                instance.equals(ModGameRules.ANTIDOTE_VESSEL_MAX_EFFECT_DURATION) ||
+                instance.equals(ModGameRules.HELIUM_FLAMINGO_FLIGHT_DURATION) ||
+                instance.equals(ModGameRules.HELIUM_FLAMINGO_RECHARGE_DURATION) ||
+                instance.equals(ModGameRules.OBSIDIAN_SKULL_FIRE_RESISTANCE_COOLDOWN) ||
+                instance.equals(ModGameRules.OBSIDIAN_SKULL_FIRE_RESISTANCE_DURATION) ||
+                instance.equals(ModGameRules.ONION_RING_HASTE_DURATION_PER_FOOD_POINT) ||
+                instance.equals(ModGameRules.ONION_RING_HASTE_LEVEL) ||
+                instance.equals(ModGameRules.CHORUS_TOTEM_TELEPORTATION_CHANCE.integerValue()) ||
+                instance.equals(ModGameRules.CLOUD_IN_A_BOTTLE_SPRINT_JUMP_VERTICAL_VELOCITY.integerValue()) ||
+                instance.equals(ModGameRules.CLOUD_IN_A_BOTTLE_SPRINT_JUMP_HORIZONTAL_VELOCITY.integerValue())) {
+            return;
+        }
+
         cir.setReturnValue(0);
     }
 }
