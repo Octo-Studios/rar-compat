8f72f33bd7174d935959c0ea9f40b1a9712e9724
SSKirillSS <SSTroveSS@Gmail.com>
2026-03-16 05:17:00 +0300

    Added Cloud in a Bottle particles

--- src/main/java/it/hurts/shatterbyte/reliquified_artifacts/network/packets/DoubleJumpPacket.java
+++ src/main/java/it/hurts/shatterbyte/reliquified_artifacts/network/packets/DoubleJumpPacket.java
@@ -4,7 +4,7 @@ import artifacts.registry.ModItems;
 import it.hurts.shatterbyte.reliquified_artifacts.ReliquifiedArtifacts;
 import it.hurts.shatterbyte.reliquified_artifacts.items.charm.CloudInBottleItem;
 import it.hurts.sskirillss.relics.utils.EntityUtils;
-import net.minecraft.core.particles.ParticleTypes;
+import it.hurts.sskirillss.relics.utils.ParticleUtils;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
@@ -12,10 +12,13 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.stats.Stats;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
 import net.neoforged.neoforge.network.handling.IPayloadContext;
 
 import javax.annotation.Nonnull;
+import java.awt.*;
 
 public class DoubleJumpPacket implements CustomPacketPayload {
     public static final Type<DoubleJumpPacket> TYPE = new Type<>(ResourceLocation.fromNamespaceAndPath(ReliquifiedArtifacts.MODID, "check_double_jump"));
@@ -47,12 +50,7 @@ public class DoubleJumpPacket implements CustomPacketPayload {
 
             Level level = player.getCommandSenderWorld();
 
-            for (int i = 0; i < 50; i++) {
-                double angle = 2 * Math.PI * i / 50;
-
-                ((ServerLevel) level).sendParticles(ParticleTypes.CLOUD, player.getX() + Math.cos(angle), player.getY(), player.getZ() + Math.sin(angle),
-                        0, 0, 0.0, 0, 0);
-            }
+            spawnCloudJumpParticles(player);
 
             level.playSound(null, player.blockPosition(), SoundEvents.WOOL_PLACE, player.getSoundSource(), 1F, 0.75F + player.getRandom().nextFloat());
         });
@@ -61,5 +59,27 @@ public class DoubleJumpPacket implements CustomPacketPayload {
     @Override
     public Type<? extends CustomPacketPayload> type() {
         return TYPE;
+    }
+
+    private static void spawnCloudJumpParticles(Player player) {
+        if (!(player.level() instanceof ServerLevel level))
+            return;
+
+        var random = player.getRandom();
+        var origin = player.position().add(0D, 0.18D, 0D);
+
+        for (var i = 0; i < 90; i++) {
+            var angle = random.nextDouble() * Math.PI * 2D;
+            var radius = Math.pow(random.nextDouble(), 0.65D) * 1.05D;
+            var x = Math.cos(angle);
+            var z = Math.sin(angle);
+
+            var radial = new Vec3(x, 0D, z);
+            var pos = origin.add(radial.scale(radius)).add(0D, (random.nextDouble() - 0.5D) * 0.18D, 0D);
+
+            var velocity = radial.scale(0.035D + random.nextDouble() * 0.04D).add(0D, 0.045D + random.nextDouble() * 0.04D, 0D);
+
+            level.sendParticles(ParticleUtils.constructSimpleSpark(new Color(255, 255, 255), 1F + random.nextFloat() * 0.5F, 20 + random.nextInt(10), 0.975F), pos.x, pos.y, pos.z, 1, velocity.x, velocity.y, velocity.z, 0D);
+        }
     }
 }
